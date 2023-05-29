19a375d5b57d8c0650caccd5620796311e943e1a
Mohammed Faris <faris.m@msf.ad>
2023-05-29 12:50:35 +0530

    shoppingcart using bloc

---
+++ lib/bloc/home_event.dart
@@ -0,0 +1,15 @@
+part of 'home_bloc.dart';
+
+abstract class HomeEvent {}
+
+class HomeInitialEvent extends HomeEvent {}
+
+class HomeProductCartButtonClickedEvent extends HomeEvent {
+  final ProductDataModel clickedProduct;
+  HomeProductCartButtonClickedEvent({required this.clickedProduct});
+}
+
+class OnCartRemove extends HomeEvent {
+  final ProductDataModel removeCart;
+  OnCartRemove({required this.removeCart});
+}
